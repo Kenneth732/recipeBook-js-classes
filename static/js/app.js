8f75c38ecf723bba304e493a5a3ed255dc45128f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-01 15:56:32 +0300

    RecipeBook for recipe empty array

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,48 @@ class Recipe {
     }
   }
   
+  class RecipeBook {
+    constructor() {
+      this.recipes = [];
+    }
+  
+
+  }
+
+  
+// data
+
+const myRecipeBook = new RecipeBook();
+
+myRecipeBook.addRecipe(
+  "Chocolate Chip Cookies",
+  ["Flour", "Sugar", "Butter", "Chocolate Chips"],
+  "1. Preheat oven to 350°F. 2. Mix all ingredients together. 3. Bake for 10-12 minutes."
+);
+
+myRecipeBook.addRecipe(
+  "Spaghetti Bolognese",
+  ["Ground beef", "Tomatoes", "Onion", "Garlic", "Spaghetti"],
+  "1. Cook spaghetti according to package instructions. 2. Brown ground beef, onion, and garlic in a pan. 3. Add tomatoes and simmer for 20 minutes. 4. Serve sauce over spaghetti."
+);
+
+myRecipeBook.viewRecipes();
+// Output:
+// Recipe: 1
+// Title: Chocolate Chip Cookies
+// Ingredients: Flour, Sugar, Butter, Chocolate Chips
+// Instructions: 1. Preheat oven to 350°F. 2. Mix all ingredients together. 3. Bake for 10-12 minutes.
+// -------------------
+// Recipe: 2
+// Title: Spaghetti Bolognese
+// Ingredients: Ground beef, Tomatoes, Onion, Garlic, Spaghetti
+// Instructions: 1. Cook spaghetti according to package instructions. 2. Brown ground beef, onion, and garlic in a pan. 3. Add tomatoes and simmer for 20 minutes. 4. Serve sauce over spaghetti.
+
+myRecipeBook.deleteRecipe(1); // Delete the second recipe
+
+myRecipeBook.viewRecipes();
+// Output:
+// Recipe: 1
+// Title: Chocolate Chip Cookies
+// Ingredients: Flour, Sugar, Butter, Chocolate Chips
+// Instructions: 1. Preheat oven to 350°F. 2. Mix all ingredients together. 3. Bake for 10-12 minutes.
