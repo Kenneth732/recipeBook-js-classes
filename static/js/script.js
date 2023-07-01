8e06df237ad784015000c95438ec4a32a719f97f
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-01 15:59:50 +0300

    Recipe constructor function

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+class Recipe {
+    constructor(title, ingredients, instructions) {
+      this.title = title;
+      this.ingredients = ingredients;
+      this.instructions = instructions;
+    }
+  
+    displayDetails() {
+      console.log("Title: " + this.title);
+      console.log("Ingredients: " + this.ingredients.join(", "));
+      console.log("Instructions: " + this.instructions);
+    }
+  }
+  
+
+  
